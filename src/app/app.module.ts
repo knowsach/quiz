ea85e3c8c150a94493a0572e5b56d413035ea63b
sachin <knowsach@gmail.com>
2020-06-28 14:56:34 +0530

    quiz ui made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { Home } from './components/home-page/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatRadioModule} from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { Home } from './components/home-page/home.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule,
+    MatRadioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home-page/home.component.ts
+++ src/app/components/home-page/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Quiz } from './Quiz.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'Home',
@@ -6,5 +8,85 @@ import { Component } from "@angular/core";
 })
 
 export class Home {
-    
+
+    quiz: Quiz[];
+    currentQuestion: Quiz;
+    selectedOption: String = null;
+    recorderdOption: any[] = new Array<any>();
+    questionIndex: number = 0;
+
+    constructor(
+        private http: HttpClient,
+    ) {
+        this.quiz = new Array<Quiz>();
+        this.currentQuestion = new Quiz();
+        this.currentQuestion.incorrect_answers = new Array<String>();
+    }
+
+    ngOnInit() {
+        this.getQuestions();
+    }
+
+    getQuestions() {
+        this.http.get(`https://opentdb.com/api.php?amount=10&type=multiple`)
+            .subscribe(
+                res => {
+                    this.quiz = res['results'];
+
+                    this.currentQuestion = this.quiz[0];
+                    console.log('quizes:', res, this.quiz, this.currentQuestion);
+                }
+            )
+    }
+
+    optionChange(event) {
+        this.selectedOption = event;
+        let answer = {
+            index: this.questionIndex,
+            option: this.selectedOption
+        }
+
+        if (this.recorderdOption.findIndex(x => x.index == this.questionIndex) > -1)
+            this.recorderdOption[this.recorderdOption.findIndex(x => x.index == this.questionIndex)] = answer;
+        else
+            this.recorderdOption.push(answer)
+
+        console.log('answer : ', this.recorderdOption);
+    }
+
+    submitQuiz() {
+
+    }
+
+    nextQuestion() {
+        ++this.questionIndex;
+        this.selectedOption = null;
+        this.currentQuestion = new Quiz();
+        Object.assign(this.currentQuestion, this.quiz[this.questionIndex]);
+        this.getMarkedAnswer();
+    }
+
+    previousQuestion() {
+        --this.questionIndex;
+        this.selectedOption = null;
+        this.currentQuestion = new Quiz();
+        Object.assign(this.currentQuestion, this.quiz[this.questionIndex]);
+        this.getMarkedAnswer();
+    }
+
+    getMarkedAnswer() {
+        let answer = this.recorderdOption.find(x => x.index == this.questionIndex);
+        if (answer)
+            this.selectedOption = answer.option;
+    }
+
+    isNextBtnActive() {
+        return this.quiz.length - 1 == this.questionIndex ? false : true;
+    }
+
+    isPreviousBtnActive() {
+        return this.questionIndex == 0 ? false : true;
+    }
+
+
 }
